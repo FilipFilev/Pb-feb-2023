fc08b03c636687c90ff8ea80dba88e40d04e60f7
Filip Filev <124343742+FilipFilev@users.noreply.github.com>
2023-02-04 13:27:57 +0200

    Concatenate

---
+++ src/L01FirstSteps040223/LAB/P06_ConcatenateData.java
@@ -0,0 +1,18 @@
+package L01FirstSteps040223.LAB;
+
+import java.util.Scanner;
+
+public class P06_ConcatenateData {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        //"You are <firstName> <lastName>, a <age>-years old person from <town>."
+        String firstName = scanner.nextLine();
+        String lastName = scanner.nextLine();
+        int age = Integer.parseInt(scanner.nextLine());
+        String town = scanner.nextLine();
+
+
+        System.out.println("You are " + firstName + " " + lastName + ", a " + age + "-years old person from " + town +".");
+    }
+}
